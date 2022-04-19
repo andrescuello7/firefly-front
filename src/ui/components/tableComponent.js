d23e42c4dd4b7dc0f521e008cd36db05f2302871
andrescuello7 <andrescuellotrabajo@gmail.com>
2022-04-19 19:59:49 +0200

    update

--- src/ui/components/tableComponent.js
+++ src/ui/components/tableComponent.js
@@ -58,16 +58,16 @@ const TableComponent = ({ progress, name, edad, image, gender, description, titl
                 </div>
                 <div className='w-100'>{inWeek ?
                     <div className='d-flex'>
-                        <button className='btn btn-warning buttonJob' onClick={CheckMethod}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" className="text-light bi bi-check-all" viewBox="0 0 16 16">
+                        <button className='btn btn-outline-success border-2 border-success buttonJob' onClick={CheckMethod}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" className="text-dark bi bi-check-all" viewBox="0 0 16 16">
                                 <path d="M8.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L2.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093L8.95 4.992a.252.252 0 0 1 .02-.022zm-.92 5.14.92.92a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 1 0-1.091-1.028L9.477 9.417l-.485-.486-.943 1.179z" />
                             </svg>
                         </button>
-                        <button className='btn btn-danger buttonJob' onClick={DeleteMethod}>
+                        {/* <button className='btn btn-success buttonJob' onClick={DeleteMethod}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
                                 <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
                             </svg>
-                        </button>
+                        </button> */}
                     </div>
                     : <div></div>
                 }</div>
