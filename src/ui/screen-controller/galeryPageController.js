2aa02b4c0ae8dd1217204693a543dacdd832e72b
andrescuello7 <andrescuellotrabajo@gmail.com>
2022-08-03 22:52:19 -0300

    update delete card of galery

--- src/ui/screen-controller/galeryPageController.js
+++ src/ui/screen-controller/galeryPageController.js
@@ -1,14 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, Button } from 'react';
 import RecuestAccess from "../../data-access/requestAccess";
-import UsersModel from '../../models/usersModel';
 import TableComponent from '../../ui/components/tableComponent';
-import { Spinner, Modal } from 'react-bootstrap'
+import { Spinner, Modal } from 'react-bootstrap';
+import axios from "axios";
+import ControllerVariables from "../../values/api";
 
 const GaleryPageController = () => {
     const [users, setUsers] = useState([]);
     const [childs, setChilds] = useState([]);
     const [images, setImages] = useState([]);
     const { getChildInProfile, getReadUser, getImagesGalery, postImagesGalery } = RecuestAccess();
+    const { api, headers, admin } = ControllerVariables();
 
     useEffect(() => {
         getReadUserController()
@@ -74,10 +76,27 @@ const GaleryPageController = () => {
         const [show, setShow] = useState(false);
         const handleClose = () => setShow(false);
         const handleShow = () => setShow(true);
+        const DeleteMethod = async (e) => {
+            e.preventDefault()
+            try {
+                await axios.delete(api + `home/images/${data._id}`, {
+                    headers,
+                });
+                window.location.reload();
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        console.log(admin);
         return (
             <div>
-                <div className='m-2' onClick={handleShow}>
-                    <img className='imagePostComponents' src={data.photo} />
+                <div className='m-2'>
+                    <button className={admin === 'true' ? "btn btn-danger w-100 d-block" : "d-none"} onClick={DeleteMethod}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
+                        </svg>
+                    </button>
+                    <img className='imagePostComponents' onClick={handleShow} src={data.photo} />
                 </div>
                 <Modal show={show} onHide={handleClose}>
                     <Modal.Header closeButton>
